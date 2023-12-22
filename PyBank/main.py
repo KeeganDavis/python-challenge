b2569f4b2e39966cba8d7bb820b0b7101dcf1d0b
KeeganDavis <keegan.davis96@gmail.com>
2023-12-22 17:47:41 -0600

    added code for analysis of pybank

---
+++ PyBank/main.py
@@ -0,0 +1,48 @@
+import os 
+import csv
+
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+
+months = 0
+total_amount = 0
+greatest_dec = 0
+greatest_inc = 0
+monthly_change = []
+previous_row = None
+
+
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    for row in csvreader:
+        months += 1
+        total_amount += int(row[1])
+        if int(row[1]) > greatest_inc:
+            inc_month = row[0]
+            greatest_inc = int(row[1])
+        if int(row[1]) < greatest_dec:
+            dec_month = row[0]
+            greatest_dec = int(row[1])
+        if previous_row != None:
+            change = int(row[1]) - previous_row
+            monthly_change.append(change)
+            previous_row = int(row[1])
+        else:
+            previous_row = int(row[1])
+
+average_change = '{:.2f}'.format(sum(monthly_change)/len(monthly_change))
+
+analysis_string = (f"Financial Analysis\n"
+      f"----------------------------\n"
+      f"Total months: {months}\n"
+      f"Total: {total_amount}\n"
+      f"Average Change: {average_change}\n"
+      f"Greatest Increase in Profits: {inc_month} ({greatest_inc})\n"
+      f"Greatest Decrease in Profits: {dec_month} ({greatest_dec})\n")
+
+print(analysis_string)
+
+        
+    
